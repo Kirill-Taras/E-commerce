9014cfc713afe7b4218932f01a90ba3ffcc20759
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-12-18 21:50:51 +0700

    Создание структуры для проекта.

---
+++ scr/classes.py
@@ -0,0 +1,12 @@
+class Category:
+    """Класс категорий"""
+
+    def __init__(self, name: str, description: str, products: list):
+        pass
+
+
+class Product:
+
+    def __init__(self, name: str, description: str, quality: int, price: float):
+        pass
+

3de9324d352d3feebc60e49fd1daa3eb0a657376
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-12-18 22:31:10 +0700

    Добавить инициализацию и атрибуты для классов.

--- scr/classes.py
+++ scr/classes.py
@@ -1,12 +1,22 @@
 class Category:
     """Класс категорий"""
 
+    count_category = 0
+    count_products = 0
+
     def __init__(self, name: str, description: str, products: list):
-        pass
+        self.name = name  # название категории
+        self.description = description  # описание категории
+        self.products = products  # товары
+        self.count_category += 1
+        self.count_products += len(products)
 
 
 class Product:
+    """Класс продуктов"""
 
     def __init__(self, name: str, description: str, quality: int, price: float):
-        pass
-
+        self.name = name  # название продукта
+        self.description = description  # описание продукта
+        self.quality = quality  # количество в наличии
+        self.price = price  # цена

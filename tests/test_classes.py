760d7a7c829472c6c9bc1a3e8c188aa432b80b81
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-12-23 19:59:55 +0700

    Выполнить дополнительное задание пот дз1

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,9 +1,11 @@
 import pytest
 
 from scr.classes import Category, Product
+from scr.config import PRODUCTS_PATH
 
 
 def test_init_category():
+    Category.count_category = 0
     category_1 = Category("Елочные игрушки", "На НГ")
     category_2 = Category("Фрукты", "Еда")
     assert category_1.name == "Елочные игрушки"
@@ -24,3 +26,17 @@ def test_init_product(test_product):
     assert test_product.quality == 2
     assert test_product.price == 100.50
     assert Category.count_products == 1
+
+
+def test_new_category():
+    Category.category_list = list()
+    Category.new_category(PRODUCTS_PATH)
+    assert Category.category_list[0].name == "Смартфоны"
+    assert Category.category_list[1].name == "Телевизоры"
+
+
+def test_new_product():
+    Category.products = list()
+    Product.new_product(PRODUCTS_PATH)
+    pr_1 = Category.products[0]
+    assert pr_1.name == "Samsung Galaxy C23 Ultra"
